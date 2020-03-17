2d342c3370d0f5406b57c5f7c3173840424c8e21
jmls9627 <jmls9627@gmail.com>
2020-03-16 21:58:12 -0400

    MathGame1

---
+++ MATH-GAME/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerHTML = 'welcome to multimath!!! Starting new game...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
